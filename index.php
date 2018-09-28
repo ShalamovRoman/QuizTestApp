ab4df97cff28374d736bc51f3ab13454fedf1653
ShalamovRoman <bringorbreak@yahoo.com>
2018-09-28 23:34:37 +0300

    Project structure added

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>QuizTest</title>
+</head>
+
+<body>
+
+<p> Wassup niggas </p>
+
+</body>
