0468971d822c0c935058a6dbbda0defb6b6f0993
ShalamovRoman <bringorbreak@yahoo.com>
2018-10-01 01:44:13 +0300

    New functions added + merged changes

--- index.php
+++ index.php
@@ -10,29 +10,115 @@ $tests = get_tests();
 
 //print_arr($tests);
 
+//Test data
+//if( isset($_GET['test']) ){
+	$test_id = 1; //(int)$_GET['test']; //test id
+	$test_data = get_test_data($test_id); //test data
+	if( is_array($test_data) ){
+		$count_questions = count($test_data); //count of questions
+	}
+//}
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>QuizTest</title>
-</head>
 
-<body>
-	
-<div class="wrap">
-	
-<?php if( $tests ): ?>
-	<h3>Варианты тестов</h3>
-	<?php foreach($tests as $test): ?>
-		<p><a href="?test=<?=$test['id']?>"><?=$test['test_name']?></a></p>
-	<?php endforeach; ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>QuizTestApp</title>
+    <meta name="description" content="description">
+    <meta name="author" content="Vasiliy">
+    <link rel="stylesheet" href="css/template.css">
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <section class="main">
+      <div class="container">
+        <div class="main__content">
+          <h3 class="main__title">Степень защищенности пользователя от социнженерных атак</h3>
+          <div class="main__grid">
+		  <?php if( $tests ): ?>
+            <div class="main__cell">
+              <div href="" class="main__button" data-modal="test1"><?=$tests[0]['test_name']?></div>
+            </div>
+            <div class="main__cell">
+              <div href="" class="main__button" data-modal="test2"><?=$tests[1]['test_name']?></div>
+            </div>
+            <div class="main__cell">
+              <div href="" class="main__button" data-modal="test3"><?=$tests[2]['test_name']?></div>
+            </div>
+          </div>
+		  <?php else: // $tests ?>
+			<h3>Нет тестов</h3>
+		  <?php endif; // $tests ?>
+          <div id="test1" class="modal">
+            <div class="modal__inner">
+              <div class="modal__close"></div>
+              <div class="test">
+                <div class="test__tab">
+                  <div class="test__question">
+                    <!-- <p class="test__description">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Власти текстами маленькая выйти силуэт жаренные свое что рот путь, не, парадигматическая взгляд родного? Моей правилами которой однажды переулка диких.</p> -->					
+					<div class="test_description">	
+							<?php if( isset($test_data) ): ?>
 
-<?php else: // $tests ?>
-	<h3>Нет тестов</h3>
-<?php endif; // $tests ?>
+								<?php if( is_array($test_data) ): ?>
 
-</div> <!-- .wrap -->
+									<p>Count of questions: <?=$count_questions?></p>
+									<br>
 
-</body>
+									<div class="test-data">
+										
+									<?php foreach($test_data as $id_question => $item): // Gett question + answers ?>
+										
+										<div class="question" data-id="<?=$id_question?>" id="question-<?=$id_question?>">
+											
+											<?php foreach($item as $id_answer => $answer): // Array questions/answers?>
+												
+												<?php if( !$id_answer ): // Print question ?>
+													<p class="q"><?=$answer?></p>
+												<?php else: // Print answers ?>
+
+									<p class="a">
+										<input type="radio" id="answer-<?=$id_answer?>" name="question-<?=$id_question?>" value="<?=$id_answer?>">
+										<label for="answer-<?=$id_answer?>"><?=$answer?></label>
+									</p>
+
+												<?php endif; // $id_answer ?>
+
+											<?php endforeach; // $item ?>
+												<br>
+										</div> <!-- .question -->
+
+									<?php endforeach; // $test_data ?>
+
+									</div> <!-- .test-data -->
+
+								<?php else: // is_array($test_data) ?>
+									Test in development
+								<?php endif; // is_array($test_data) ?>
+
+							<?php endif; // isset($test_data) ?>
+					</div>					
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="info" class="modal">
+            <div class="modal__inner">
+              <div class="modal__close"></div>
+              <p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Выйти точках деревни семантика заголовок заглавных переписывается рыбными вскоре, текстами что, образ залетают необходимыми дороге. Решила переписывается все, буквенных наш!</p>
+              <p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Выйти точках деревни семантика заголовок заглавных переписывается рыбными вскоре, текстами что, образ залетают необходимыми дороге. Решила переписывается все, буквенных наш!</p>
+            </div>
+          </div>
+          <div class="main__info" data-modal="info">Инфо</div>
+        </div>
+      </div>
+    </section>
+  </body>
 </html>
--- php/functions.php
+++ php/functions.php
@@ -21,3 +21,24 @@ function get_tests(){
 	}
 	return $data;
 }
+
+/**
+* Getting test data
+**/
+function get_test_data($test_id){
+	if( !$test_id ) return;
+	global $db;
+	$query = "SELECT q.question_text, q.parent_test, a.id, a.answer_text, a.parent_question
+		FROM questions q
+		LEFT JOIN answers a
+			ON q.id = a.parent_question
+				WHERE q.parent_test = $test_id";
+	$res = mysqli_query($db, $query);
+	$data = null;
+	while($row = mysqli_fetch_assoc($res)){
+		if( !$row['parent_question'] ) return false;
+		$data[$row['parent_question']][0] = $row['question_text'];
+		$data[$row['parent_question']][$row['id']] = $row['answer_text'];
+	}
+	return $data;
+}
