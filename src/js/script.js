5902e49c802893e14c1377749d9e10905fd12858
vasilstar <vasilstar97@gmail.com>
2018-10-10 00:11:12 +0500

    смотри index.html

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
   });
   $('.modal__close').click(function(){
     $(this).closest('.modal').removeClass('js-active');
+    $(this).closest('.modal').find('.test__tab').removeClass('js-active').first().addClass('js-active');
+  });
+  $('.test').each(function() {
+    var test = $(this);
+    var current = test.find('.test__tab.js-active');
+    var next = test.find('.test__tab.js-active+.test__tab');
+    current.find('.test__next').click(function() {
+      current.removeClass('js-active');
+      next.addClass('js-active');
+    });
   });
 });
